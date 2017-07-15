1c860159946e692bdf6a4c1afbf0d639fb912656
Maciej Mionskowski <business@maciekmm.net>
2017-07-15 22:48:57 +0200

    Added logger

--- backend/main.go
+++ backend/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"os"
 
 	"github.com/AllegroTechDays/poz_Two_Tired/backend/converter"
 )
 
 func main() {
+	logger := log.New(os.Stdout, "TwoTired", log.Ldate|log.Lshortfile)
 	data, err := converter.Load("./_data")
 	if err != nil {
-		panic(err)
+		logger.Fatal(err.Error())
 	}
 	for _, activity := range data {
 		fmt.Println(activity.Hash())
