f78a9101e4cc00d2818661c3e9d48ec5cc93d43c
Marek Kochanowski <marek@kochanow.ski>
2017-07-16 00:55:38 +0200

    Upload dist/

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,15 +11,12 @@
 </template>
 
 <script>
-require('material-design-lite')
 export default {
   name: 'app'
 }
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
-@import url('https://code.getmdl.io/1.2.1/material.blue-red.min.css');
 body {
   margin: 0;
 }
